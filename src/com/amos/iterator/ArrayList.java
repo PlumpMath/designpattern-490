5594fc29b8d6473ac73185f5f7d51e9b1d7ae762
liyayun <amosli@juxinli.com>
2015-02-10 20:03:14 +0800

    自己动手实现ArrayList 和 LinkedList

---
+++ src/com/amos/iterator/ArrayList.java
@@ -0,0 +1,23 @@
+package com.amos.iterator;
+
+public class ArrayList {
+	Object[] objs = new Object[10];
+	int index =0;
+	public void add(Object o){
+		if(index==objs.length){
+			Object[] newObjs = new Object[objs.length*2];
+			System.arraycopy(objs, 0, newObjs, 0, objs.length);
+			
+			objs = newObjs;
+		}
+
+		objs[index] = o;
+		
+		index++;
+	}
+	
+	public int size(){
+		return index;
+	}
+	
+}
---
+++ src/com/amos/iterator/Test.java
@@ -0,0 +1,22 @@
+package com.amos.iterator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// test1
+		// ArrayList list = new ArrayList();
+		// for(int i=0;i<30;i++){
+		// list.add(i);
+		// }
+		// System.out.println(list.size());
+		//
+
+		// test2
+		LinkedList list = new LinkedList();
+		for (int i = 0; i < 30; i++) {
+			list.add(i);
+		}
+		System.out.println(list.size());
+
+	}
+}
